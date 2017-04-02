bb0ea425ec1ce5f2392cd2d28583980645c30d93
Hugo Maia <hugofemaia@gmail.com>
2017-04-02 09:33:38 +0100

    Added ios platform code generated

---
+++ platforms/ios/build/Groceries.build/Debug-iphonesimulator/Groceries.build/Objects-normal/i386/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/hugomaia/Documents/projects/nativescript/Groceries/platforms/ios/internal/main.m \
+  /Users/hugomaia/Documents/projects/nativescript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/hugomaia/Documents/projects/nativescript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/hugomaia/Documents/projects/nativescript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/hugomaia/Documents/projects/nativescript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/hugomaia/Documents/projects/nativescript/Groceries/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/hugomaia/Documents/projects/nativescript/Groceries/platforms/ios/internal/TNSDebugging.h
